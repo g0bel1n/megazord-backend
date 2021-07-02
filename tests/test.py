77b2b67bf7e446cb53281f9f5978bff04a7a62b9
Lucas Saban <lucas.saban@icloud.com>
2021-07-02 15:41:56 +0200

    fixes

--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,19 @@
 from megazord_project.utilitaries.utils import *
 
 
-def test_listdir_nohidden_3(path="data_test"):
+def test_listdir_nohidden_3(path="../data_test"):
     assert listdir_nohidden(path, jpg_only=False) == ['ball_bearing', 'handle', 'wheel']
 
 
-def test_listdir_nohidden_0(path="data_test"):
+def test_listdir_nohidden_0(path="../data_test"):
     assert listdir_nohidden(path, jpg_only=True) == []
 
 
-def test_data_repartition_zord(path="data_test"):
+def test_data_repartition_zord(path="../data_test"):
     assert data_repartition("wheel", path) == [2, 3, 2]
 
 
-def test_data_repartition_main_zord(path="data_test"):
+def test_data_repartition_main_zord(path="../data_test"):
     assert data_repartition("main_zord", path) == [6, 6, 7]
 
 
