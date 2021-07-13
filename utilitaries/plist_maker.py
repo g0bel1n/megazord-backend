69ad8cb3092c5b44f45b2a42c5125800c145637b
Lucas Saban <lucas.saban@icloud.com>
2021-07-13 17:10:43 +0200

    added features on the plist file

--- utilitaries/plist_maker.py
+++ utilitaries/plist_maker.py
@@ -1,13 +1,22 @@
 import plistlib
-import numpy as np
-from  utils import int_reader
+import pandas as pd
+from utils import int_reader
 
+df = pd.read_csv("/Users/lucas/swiss_knife/materiel.csv", delimiter=",")
+df.set_index(["label"], inplace=True)
 dict = int_reader("label")
 root = {}
+df["description"]=df["description"].astype(str)
+print(df["description"])
 for key in dict:
-    temp_dic = {"label" : key, "description": "bla bla bla"}
-    root[key] = temp_dic
+    label, description, url = (df["Nom appli"].loc[key],
+                               "Référence : " + str(df["ref"].loc[key]) +"\nFournisseur : "+
+                                                    df["fournisseur"].loc[key] +"\n" +
+                                                    ("Description: "+df["description"].loc[key] if not df["description"].loc[key]=="nan" else ""),
+                               df["url"].loc[key]) if key in df.index else (key, "")
 
+    temp_dic = {"label": label, "description": description, "url" : url}
+    root[key] = temp_dic
 
 with open("ProductCatalog.plist", 'wb') as fp:
     plistlib.dump(root, fp)
